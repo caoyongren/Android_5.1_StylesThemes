8cddb123902b93f21e637a3e3499db32cb09dd9c
CaoYongren <m18410261910@163.com>
2017-06-03 15:09:18 +0800

    innotation
    Signed-off-by: CaoYongren <m18410261910@163.com>

--- app/src/main/java/com/example/application/android_51_stylesthemes/MainActivity.java
+++ app/src/main/java/com/example/application/android_51_stylesthemes/MainActivity.java
@@ -11,3 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_styled);
     }
 }
+/*
+ * style.xml属性parent:
+ *   就是继承： 对父类进行扩展；
+ *
+ *   <style name="BaseStyle" />
+ *   <style name="NewStyle" parent="BaseStyle" />
+ *   NewStyle就是BaseStyle的扩展。
+ * */
